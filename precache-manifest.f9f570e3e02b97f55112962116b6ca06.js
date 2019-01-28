3f6a47bd6fb7e998f5ef842066c207224b7aa3d4
hifebriansyah <hifebriansyah@gmail.com>
2019-01-28 07:19:29 +0700

    Updates

---
+++ precache-manifest.f9f570e3e02b97f55112962116b6ca06.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "8f9171c835b84e8cc6f5",
+    "url": "/static/js/main.8f9171c8.chunk.js"
+  },
+  {
+    "revision": "9e28379229da9a4bd7a2",
+    "url": "/static/js/1.9e283792.chunk.js"
+  },
+  {
+    "revision": "8f9171c835b84e8cc6f5",
+    "url": "/static/css/main.3d095d64.chunk.css"
+  },
+  {
+    "revision": "349eb8bf1207c7945e18aacabdbdf85c",
+    "url": "/index.html"
+  }
+];
